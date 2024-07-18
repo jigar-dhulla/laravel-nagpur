09a171b0cbf3f4106ee2340d293ec0b5a221f1ca
Jigar Dhulla <jigar.d@trandingdesk.dk>
2024-07-18 12:04:56 +0530

    Real-time updates on Public Channel

--- app/Filament/Pages/KanbanBoard.php
+++ app/Filament/Pages/KanbanBoard.php
@@ -2,12 +2,35 @@
 
 namespace App\Filament\Pages;
 
+use App\Events\OnTaskStatusChange;
 use App\Models\Task;
 use App\TaskStatus;
+use Livewire\Attributes\On;
 use Mokhosh\FilamentKanban\Pages\KanbanBoard as BaseKanbanBoard;
 
 class KanbanBoard extends BaseKanbanBoard
 {
     protected static string $model = Task::class;
     protected static string $statusEnum = TaskStatus::class;
+
+    public function onStatusChanged(int $recordId, string $status, array $fromOrderedIds, array $toOrderedIds): void
+    {
+        parent::onStatusChanged($recordId, $status, $fromOrderedIds, $toOrderedIds);
+        OnTaskStatusChange::dispatch($recordId, $status, $fromOrderedIds, $toOrderedIds);
+    }
+
+    public function onSortChanged(int $recordId, string $status, array $orderedIds): void
+    {
+        parent::onSortChanged($recordId, $status, $orderedIds);
+        OnTaskStatusChange::dispatch($recordId, $status, $orderedIds, $orderedIds);
+    }
+
+    #[On('echo:kanban-board,OnTaskStatusChange')]
+    #[On('echo:kanban-board,.TaskUpdated')]
+    #[On('echo:kanban-board,.TaskCreated')]
+    #[On('echo:kanban-board,.TaskDeleted')]
+    public function refreshBoard($event): void
+    {
+    }
+
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Database\Eloquent\BroadcastsEvents;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\EloquentSortable\Sortable;
@@ -10,6 +12,14 @@ use Spatie\EloquentSortable\SortableTrait;
 class Task extends Model implements Sortable
 {
     use SortableTrait;
+    use BroadcastsEvents;
 
     use HasFactory;
+
+    public function broadcastOn($event)
+    {
+        return [
+            new Channel('kanban-board'),
+        ];
+    }
 }
